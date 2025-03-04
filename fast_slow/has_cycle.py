d28cbaf1778f62870e20d1d6add848104b7294f2
Om Balapure <odbalapure@gmail.com>
2025-03-05 00:42:44 +0530

    Add comments

--- fast_slow/has_cycle.py
+++ fast_slow/has_cycle.py
@@ -1,10 +1,33 @@
 from Node import *
 
+def has_cycle_brute(head: LinkedListNode) -> bool:
+    """
+    Does a linked list have cycle
+    :head LinkedListNode: Head of linked list using a hashset
+    :return: bool
+
+    COMPLEXITY
+    Time: O(n); since we traverse the entire linked list
+    Space: O(n); in worst case we are storing every node in the linked list in the hashset
+    """
+    st = set()
+    temp: LinkedListNode = head
+    while temp:
+        if temp in st:
+            return True
+        st.add(temp)
+        temp = temp.next
+    return False
+
 def has_cycle(head: LinkedListNode) -> bool:
     """
     Does a linked list have cycle
-    :head LinkedListNode: Head of linked list
+    :head LinkedListNode: Head of linked list using two pointers
     :return: bool
+
+    COMPLEXITY
+    Time: O(n); n is the no. of nodes in the linked list
+    Space: O(1) 
     """
     slow = fast = head
     while fast and fast.next:
--- fast_slow/middle_of_linked_list.py
+++ fast_slow/middle_of_linked_list.py
@@ -1,10 +1,36 @@
 from Node import *
+from typing import Tuple
+
+def createList(head: LinkedListNode) -> Tuple[list, int]:
+    temp: LinkedListNode = head
+    arr: list = []
+    while temp:
+        arr.append(temp)
+        temp = temp.next
+    return (arr, len(arr))
+        
+def middle_of_linked_list_brute(head: LinkedListNode) -> LinkedListNode:
+    """
+    Find middle of a linkedlist
+    :head LinkedListNode: Head of linked list
+    :return: LinkedListNode
+
+    COMPLEXITY
+    Time: O(n); since we iterate over each node
+    Space: O(n); since we are storing each node
+    """
+    arr, n = createList(head)
+    return arr[n//2]
 
 def middle_of_linked_list(head: LinkedListNode) -> LinkedListNode:
     """
     Find middle of a linkedlist
     :head LinkedListNode: Head of linked list
     :return: LinkedListNode
+
+    COMPLEXITY
+    Time: O(n); since we iterate over each node
+    Space: O(n); since we are storing each node
     """
     slow = fast = head
     while fast and fast.next:
